b6ea05e10017148f97f6ca02b754dc9430a5e081
MarwanGamal <marawangamal000@gmail.com>
2026-03-13 22:09:53 +0200

    add the header and landing page to the app,

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
+import { Landing } from './features/landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Landing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
